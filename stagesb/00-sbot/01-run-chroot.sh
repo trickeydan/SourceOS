4dcae8fb39b2acd95a0847260e189afeacfe6799
Dan Trickey <dan@trickey.io>
2019-08-01 19:23:49 +0100

    Add precompiled wheel

--- stagesb/00-sbot/01-run-chroot.sh
+++ stagesb/00-sbot/01-run-chroot.sh
@@ -1,1 +1,5 @@
+wget https://j5.org.uk/opencv_contrib_python_headless-4.1.0.25-cp37-cp37m-linux_armhf.whl
+
+pip3 install opencv_contrib_python_headless-4.1.0.25-cp37-cp37m-linux_armhf.whl
+
 pip3 install sbot numpy scipy
