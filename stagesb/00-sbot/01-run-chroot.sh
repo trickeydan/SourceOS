4946a0a921898712bb0f674836c180a71990082a
Dan Trickey <trickeydan@users.noreply.github.com>
2019-08-02 20:59:43 +0100

    Pin to old sbot for testing. Remove bad wheel

--- stagesb/00-sbot/01-run-chroot.sh
+++ stagesb/00-sbot/01-run-chroot.sh
@@ -1,5 +1,1 @@
-wget https://j5.org.uk/opencv_contrib_python_headless-4.1.0.25-cp37-cp37m-linux_armhf.whl
-
-pip3 install opencv_contrib_python_headless-4.1.0.25-cp37-cp37m-linux_armhf.whl
-
-pip3 install sbot numpy scipy
+pip3 install sbot==0.2.0 numpy scipy
