b3d0957ed1a3ad8196e256546c37f96d7cc61c91
Dan Trickey <trickeydan@users.noreply.github.com>
2019-08-03 10:37:56 +0100

    Yes, I do want the debs

--- stagesb/00-debpackages/01-run-chroot.sh
+++ stagesb/00-debpackages/01-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Note that the order here is important.
-apt install /home/pi/usbmount*.deb
-apt install /home/pi/runusb*.deb
+apt install -y /home/pi/usbmount*.deb
+apt install -y /home/pi/runusb*.deb
