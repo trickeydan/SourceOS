f52a3312baa60733f5674ce37ae3acfce4ee7715
Dan Trickey <trickeydan@users.noreply.github.com>
2019-08-03 10:12:40 +0100

    Reorder deb install.

--- stagesb/00-debpackages/01-run-chroot.sh
+++ stagesb/00-debpackages/01-run-chroot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-for package in /home/pi/*.deb;
-do
-	apt install $package
-done
+# Note that the order here is important.
+apt install /home/pi/usbmount*.deb
+apt install /home/pi/runusb*.deb
