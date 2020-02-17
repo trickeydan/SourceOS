a8629a6d9f6463859b710ae366b4aed82d0f66c2
Dan Trickey <dan@trickey.io>
2020-02-17 18:51:44 +0000

    Add packages

--- stagesb/00-debpackages/01-run-chroot.sh
+++ stagesb/00-debpackages/01-run-chroot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-apt install -y /home/pi/usbmount*.deb
-apt install -y /home/pi/runusb*.deb
+# apt install -y /home/pi/usbmount*.deb
+# apt install -y /home/pi/runusb*.deb
--- stagesb/00-sbot/01-run-chroot.sh
+++ stagesb/00-sbot/01-run-chroot.sh
@@ -1,3 +1,11 @@
-apt install -y libatlas-base-dev
-
-pip3 install sbot numpy
+pip3 install \
+cached-property==1.5.1 \
+j5==0.9.1 \
+numpy==1.18.1 \
+opencv-contrib-python-headless==4.0.1.24 \
+pyquaternion==0.9.5 \
+pyserial==3.4 \
+pyusb==1.0.2 \
+sbot==0.8.0 \
+typing-extensions==3.7.4.1 \
+zoloto==0.5.2
