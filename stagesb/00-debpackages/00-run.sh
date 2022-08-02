eda969369b6fb2a2e2cbe7cc6e71eead1dcf3a0d
Tommy Poll <tommy@tommy.moe>
2022-08-02 15:27:35 +0100

    Build submodules properly

--- stagesb/00-debpackages/00-run.sh
+++ stagesb/00-debpackages/00-run.sh
@@ -1,9 +1,7 @@
 cd $BASE_DIR/packages/runusb
 debuild -uc -us
-mv build/*.deb $BASE_DIR/work/*-SourceOS/stagesb/rootfs/home/pi
 
 cd $BASE_DIR/packages/usbmount
 dpkg-buildpackage -us -uc -b
-mv build/*.deb $BASE_DIR/work/*-SourceOS/stagesb/rootfs/home/pi
 
-# mv $BASE_DIR/packages/built/*.deb $BASE_DIR/work/*-SourceOS/stagesb/rootfs/home/pi
+mv $BASE_DIR/packages/*.deb $BASE_DIR/work/*-SourceOS/stagesb/rootfs/home/pi
