85d055edb8acbc28d082037c670e15df4d658b16
Tomii Poll <tommy@tommy.moe>
2022-08-07 17:51:03 +0100

    Update j5, sbot, j5-zoloto

--- stagesb/00-sbot/01-run-chroot.sh
+++ stagesb/00-sbot/01-run-chroot.sh
@@ -1,12 +1,12 @@
 pip3 install \
 cached-property==1.5.2 \
-j5==0.13.1 \
+j5==1.0.0.rc1 \
 numpy==1.19.5 \
 pyquaternion==0.9.9 \
 pyserial==3.5 \
 pyusb==1.2.1 \
-sbot==0.9.0 \
+sbot==0.10.1 \
 typing-extensions==4.1.1 \
 opencv-contrib-python-headless==4.4.0.46 \
 zoloto==0.8.0
-j5-zoloto===0.2.0 \
+j5-zoloto===0.3.0 \
